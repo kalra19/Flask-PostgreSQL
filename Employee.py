d10c1bfde6055ff5b4bcc2c6d4136a9b4566d0dd
kalra19 <jack70019@gmail.com>
2019-08-14 00:32:42 +0530

    Initial Commit

--- Employee.py
+++ Employee.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/mydatabase'
 
 db = SQLAlchemy(app)
 api = Api(app)
